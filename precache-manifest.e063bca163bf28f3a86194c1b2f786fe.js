de7fd601c4b7d9ec28d37161a90a0d67740a8586
goodwinamundson <goodwinamundson@gmail.com>
2022-05-16 02:53:19 +0200

    Updates

---
+++ precache-manifest.e063bca163bf28f3a86194c1b2f786fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9eb1f259dc08d6ecff9153abaf54ed5",
+    "url": "/goodwinamundson/react-portfolio/index.html"
+  },
+  {
+    "revision": "ed239fa8577fb82a4195",
+    "url": "/goodwinamundson/react-portfolio/static/css/main.b00df02e.chunk.css"
+  },
+  {
+    "revision": "985e6474bca378fc61f3",
+    "url": "/goodwinamundson/react-portfolio/static/js/2.6fc13e89.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/goodwinamundson/react-portfolio/static/js/2.6fc13e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed239fa8577fb82a4195",
+    "url": "/goodwinamundson/react-portfolio/static/js/main.6b77196f.chunk.js"
+  },
+  {
+    "revision": "ef54414142720f8b0e30",
+    "url": "/goodwinamundson/react-portfolio/static/js/runtime-main.adbda42f.js"
+  },
+  {
+    "revision": "1bf86a449aea6c0781785a09b5fe93c5",
+    "url": "/goodwinamundson/react-portfolio/static/media/Resume.1bf86a44.pdf"
+  },
+  {
+    "revision": "6992d68740045aa017d4eaa0598f4a50",
+    "url": "/goodwinamundson/react-portfolio/static/media/aboutMe.6992d687.jpg"
+  },
+  {
+    "revision": "55cace6cdd0bad56d449c640070ae954",
+    "url": "/goodwinamundson/react-portfolio/static/media/background1.55cace6c.jpg"
+  },
+  {
+    "revision": "e2e7f2ef4459ac2f46781076fae1543d",
+    "url": "/goodwinamundson/react-portfolio/static/media/project1.e2e7f2ef.jpg"
+  },
+  {
+    "revision": "5b2dcaedbfb25b4c4c12b1d79033f1bc",
+    "url": "/goodwinamundson/react-portfolio/static/media/project2.5b2dcaed.jpg"
+  }
+]);
